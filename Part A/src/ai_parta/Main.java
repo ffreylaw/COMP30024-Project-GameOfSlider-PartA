d39463513c4735cc5cff09c053d2ec50abe54a8a
ffreylaw <ffreylaw@gmail.com>
2017-03-30 18:22:20 +1100

    update

--- Part A/src/ai_parta/Main.java
+++ Part A/src/ai_parta/Main.java
@@ -1,26 +1,20 @@
 package ai_parta;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
 	
+	private static Scanner reader;
+
 	public static void main(String[] argv) {
-		Scanner reader = null;
-		try {
-			reader = new Scanner(new File("src/input.txt"));
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		reader = new Scanner(System.in);
 		
 		int size = reader.nextInt();
 		Board board = new Board(size);
 		
 		reader.nextLine();
 		int row = 0;
-		while (reader.hasNextLine()) {
+		while (row < size && reader.hasNextLine()) {
 			String line = reader.nextLine();
 			for (int col = 0; col < line.length(); col++) {
 				board.setBoard(row, col, line.charAt(col));
